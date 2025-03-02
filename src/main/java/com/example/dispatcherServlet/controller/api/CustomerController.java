b45ca7cdffbef235ea6bb812b4a0647c705f6016
ITWeiss <gottfried98@bk.ru>
2025-03-02 20:55:51 +0700

    DispatcherServlet

--- src/main/java/com/example/dispatcherServlet/controller/api/CustomerController.java
+++ src/main/java/com/example/dispatcherServlet/controller/api/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.dispatcherServlet.controller.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,9 @@ import java.util.List;
 public class CustomerController {
 
     @GetMapping(value = "/customers")
-    public List<String> getCustomers() {
-        return List.of("Customer1", "Customer2", "Customer3");
+    public ResponseEntity<List<String>> getCustomers() {
+        List<String> customers = List.of("Customer1", "Customer2", "Customer3");
+        return ResponseEntity.ok(customers);
     }
 
 }
